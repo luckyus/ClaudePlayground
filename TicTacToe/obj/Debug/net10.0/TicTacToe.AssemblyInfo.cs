6d25346d9c2fcfd6352a9560eb80a01b86bda65a
Brian Leung <brian@lucky.com.hk>
2026-04-21 21:52:37 +0800

    blah blah blah

--- TicTacToe/obj/Debug/net10.0/TicTacToe.AssemblyInfo.cs
+++ TicTacToe/obj/Debug/net10.0/TicTacToe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7dde30f8e7702d0ca0de11c380e54092f75cb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ba72225021ed149cf66362ca70f0115c452846")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
